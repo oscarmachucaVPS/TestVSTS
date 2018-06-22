fadf92247410a8f3a19d27b4020a3afafafb565f
oscarmachucaVPS <oscarmachuca@valuepaymentsystems.com>
2018-06-22 10:04:34 -0500

    Created Unit Test for testing CI.

---
+++ AWS.ProjtwoTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AWS.Projtwo.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.Projtwo.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
