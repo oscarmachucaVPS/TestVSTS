dd0e9116f1d88b5f09e36a59a754be3a26b65c24
oscarmachucaVPS <oscarmachuca@valuepaymentsystems.com>
2018-06-22 11:26:03 -0500

    Commit to Trigger CI in VSTS with fail test. And try to send email.

--- AWS.Projtwo/Controllers/HomeController.cs
+++ AWS.Projtwo/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace AWS.Projtwo.Controllers
         {
             var version = Assembly.GetExecutingAssembly().GetName().Version;
 
-            //Change to test CI Triggering
+            //Change to test CI Triggering 2
             ViewBag.WEbConfigOscar = WebConfigurationManager.AppSettings["WebTransformOscar"];
             return View();
         }
